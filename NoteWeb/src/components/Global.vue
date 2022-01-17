c13575225c181795215e2e9146cc3605fdd7069f
MYSOFT\guojh03 <guojh03@mingyuanyun.com>
2022-01-17 18:06:01 +0800

    前端调用后端url修改

--- NoteWeb/src/components/Global.vue
+++ NoteWeb/src/components/Global.vue
@@ -1,5 +1,13 @@
+<!--
+ * @Author: your name
+ * @Date: 2022-01-15 22:03:58
+ * @LastEditTime: 2022-01-17 17:50:29
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \NoteWeb\src\components\Global.vue
+-->
 <script>
- const BASE_URL = 'https://localhost:8081/api/';
+ const BASE_URL = 'https://101.35.83.57:5001/api/';
  export default{
   BASE_URL
  }
