de337b75cdb9b62fc7b716e2176150988aa0f302
qiuxuandong <924820364@qq.com>
2022-12-21 20:10:35 +0800

    perf(login refactor): simple verification and login style refactor

--- NoteWeb/src/components/Login.vue
+++ NoteWeb/src/components/Login.vue
@@ -8,66 +8,25 @@
 -->
 
 <template>
-  <!-- <div id="login"> -->
-  <!-- <img alt="登录页面" src="../assets/NoteLog.png">
-  <el-form
-    :model="ruleForm2"
-    status-icon
-    :rules="rules2"
-    ref="ruleForm2"
-    label-width="100px"
-    class="loginRuleForm"
-  >
-  <el-form-item label="账号" prop="account" >
-      <el-input
-        type="text"
-        v-model="ruleForm2.UserName"
-        auto-complete="off"
-      ></el-input>
-    </el-form-item>
-    <el-form-item label="密码" prop="pass">
-      <el-input
-        type="password"
-        v-model="ruleForm2.password"
-        auto-complete="off"
-      ></el-input>
-    </el-form-item>
-    <el-form-item label="确认密码" prop="checkPass">
-      <el-input
-        type="password"
-        v-model="ruleForm2.checkPass"
-        auto-complete="off"
-      ></el-input>
-    </el-form-item>
-    <el-form-item label="年龄" prop="age">
-      <el-input v-model.number="ruleForm2.age"></el-input>
-    </el-form-item>
-    <el-form-item>
-      <el-button type="primary" @click="submitForm('ruleForm2')"
-        >提交</el-button
-      >
-      <el-button @click="resetForm('ruleForm2')">重置</el-button>
-    </el-form-item>
-  </el-form>
-</div> -->
+<!-- todo: recode verification function -->
 <div class="cardcontainer">
   <div class="box login">
-    <div class="form-content">
+    <div class="form-content" :rules="rules2">
       <div class="avtar">
         <div class="pic"><img src="../assets/1.jpg" alt=""></div>
       </div>
       <h1>Welcome back</h1>
-      <form action="#" class="form"  ref="ruleForm2">
+      <form action="#" class="form"  ref="ruleForm2" :model="ruleForm2" :rules="rules2">
           <div>
             <i class="fa fa-user-o"></i>
             <input type="text" placeholder="username" v-model="ruleForm2.UserName">
           </div>
           <div>
             <i class="fa fa-key"></i>
-            <input type="password" placeholder="password"  v-model="ruleForm2.password">
+            <input type="password" placeholder="password" v-model="ruleForm2.password">
           </div>
           <div class="btn">
-            <button  @click="submitForm('ruleForm2')">login</button>
+            <button  @click="submitForm()">login</button>
           </div>
       </form>
       <p class="btn-something">
@@ -268,64 +227,12 @@ svg {
 const axios = require('axios');
 export default {
   data() {
-    // 年龄校验
-    var checkAge = (rule, value, callback) => {
-      if (!value) {
-        return callback(new Error("年龄不能为空"));
-      }
-      setTimeout(() => {
-        if (!Number.isInteger(value)) {
-          callback(new Error("请输入数字值"));
-        } else {
-          if (value < 18) {
-            callback(new Error("必须年满18岁"));
-          } else {
-            callback();
-          }
-        }
-      }, 1000);
-    };
-    // 也用户名校验
-     var validateAccount = (rule, value, callback) => {
-      if (value === "") {
-        callback(new Error("请输入账号"));
-      } else {
-        callback();
-      }
-    };
-    // 密码校验
-    var validatePass = (rule, value, callback) => {
-      if (value === "") {
-        callback(new Error("请输入密码"));
-      } else {
-        if (this.ruleForm2.checkPass !== "") {
-          this.$refs.ruleForm2.validateField("checkPass");
-        }
-        callback();
-      }
-    };
-    // 二次密码校验
-    var validatePass2 = (rule, value, callback) => {
-      if (value === "") {
-        callback(new Error("请再次输入密码"));
-      } else if (value !== this.ruleForm2.password) {
-        callback(new Error("两次输入密码不一致!"));
-      } else {
-        callback();
-      }
-    };
     return {
       ruleForm2: {
         UserName: "",
         password: "",
         checkPass: "",
         age: ""
-      },
-      rules2: {
-        account: [{ validator: validateAccount, trigger: "blur" }],
-        pass: [{ validator: validatePass, trigger: "blur" }],
-        checkPass: [{ validator: validatePass2, trigger: "blur" }],
-        age: [{ validator: checkAge, trigger: "blur" }]
       },
       userInfo: {
         UserGUID: "330DA93D-A581-450B-9109-E07947349A20",
@@ -357,46 +264,34 @@ export default {
   },
   methods: {
     // 提交
-    submitForm(formName) {
+    submitForm() {
+    if (this.ruleForm2.UserName && this.ruleForm2.password) {
       var me = this;
-      me.$refs[formName].validate(valid => {
-        if (valid) {
-          axios({
-                method: 'post',
-                url: '/MyUser/LoginIndex',
-                contentType: "application/json; charset=utf-8",
-                data: JSON.stringify(this.ruleForm2)
-          }).then(function(res) {
-            if (res.data.Status === 200) {
-              localStorage.setItem('loginInfo', JSON.stringify(res.data));
-              me.$router.push({path: '/home'});
-            } else {
-              alert("error submit2!");
-            }
-          })
+      axios({
+        method: 'post',
+        url: '/MyUser/LoginIndex',
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify(this.ruleForm2)
+      }).then(function(res) {
+        if (res.data.Status === 200) {
+          localStorage.setItem('loginInfo', JSON.stringify(res.data));
+          me.$router.push({path: '/home'});
         } else {
-          console.log("error submit!!");
-          return false;
+          alert("error submit2!");
         }
       });
-    },
-    resetForm(formName) {
+    }
+    this.errors = [];
+    if (!this.ruleForm2.UserName) {
+      this.errors.push('Name required');
+    }
+    if (!this.ruleForm2.password) {
+      this.errors.push('password required');
+    }
+  },
+  resetForm(formName) {
       this.$refs[formName].resetFields();
     }
   }
 };
 </script>
-
-<!-- <style>
-#login {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin: 0 auto;
-  width:600px;
-  margin-top: 60px;
-}
-
-</style> -->
